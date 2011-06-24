af96c741a88e6975d506ca0bf19d66c4b56637d0
Kyle Wilkinson <kai@wikyd.org>
2011-06-24 01:29:01 -0700

    Fix up namespacing for generator and filenames.

---
+++ lib/rails/generators/refinery/settings_initializer_generator.rb
@@ -0,0 +1,20 @@
+module Refinery
+  module Generators
+    class SettingsInitializerGenerator < Rails::Generators::Base
+      desc 'Creates a RefinerySetting initializer to load RefinerySettings from a configuration file.'
+
+      def self.source_root
+        @_refinerycms_settigs_initializer_source_root ||= File.expand_path("../templates", __FILE__)
+      end
+
+      def create_config_file
+        template 'settings.yml', File.join('config', 'refinery', 'settings.yml')
+      end
+
+      def create_initializer_file
+        template 'refinerycms_settings.rb', File.join('config', 'initializers', 'refinerycms_settings.rb')
+      end
+    end
+  end
+end
+
